2086c81ae0d5d86e112687b532c6d4dbd9c67009
vtiwari2 <vtiwari2@LIBLAPTOP373>
2021-11-20 23:23:47 -0500

    Fixed the program output structure as per the project

--- Program.cs
+++ Program.cs
@@ -7,96 +7,56 @@ namespace Algo_Shortest_Paths_in_a_Network
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to My Shortest Path Project,please run the command as in the read me file.");
-
             Operations operations = new Operations();
 
-            var option = operations.ChooseOption();
+            Console.WriteLine("Welcome to My Shortest Path Project,please run the command as in the read me file.");
 
-            while (option != null)
+            while (true)
             {
+                var input = Console.ReadLine();
+
+                string[] inputCase = input.Split(" ");
+
                 try
                 {
-                    switch (option)
+                    switch (inputCase[0])
                     {
-                        case "1":
-                            Console.WriteLine("Enter in format, graph <filename>.txt");
-                            var inputCase1 = Console.ReadLine();
-                            operations.BuildGraph(inputCase1);
+                        case "graph":
+                            operations.BuildGraph(input);
                             break;
 
-                        case "2":
-                            Console.WriteLine("Enter in format, addedge tailvertex headvertex transmit time");
-                            var inputCase2 = Console.ReadLine();
-                            string[] inputFor2 = inputCase2.Split(" ");
-                            operations.AddAnEdge(inputFor2[1], inputFor2[2], float.Parse(inputFor2[3]));
+                        case "addedge":
+                            operations.AddAnEdge(inputCase[1], inputCase[2], float.Parse(inputCase[3]));
                             break;
 
-                        case "3":
-                            Console.WriteLine("Enter in format,deleteedge tailvertex headvertex");
-                            var inputCase3 = Console.ReadLine();
-                            string[] inputFor3 = inputCase3.Split(" ");
-                            operations.DeleteAnEdge(inputFor3[1], inputFor3[2]);
+                        case "deleteedge":
+                            operations.DeleteAnEdge(inputCase[1], inputCase[2]);
                             break;
 
-                        case "4":
-                            Console.WriteLine("Enter in format,edgedown tailvertex headvertex");
-                            var inputCase4 = Console.ReadLine();
-                            string[] inputFor4 = inputCase4.Split(" ");
-                            operations.EdgeDown(inputFor4[1], inputFor4[2]);
+                        case "edgedown":
+                            operations.EdgeDown(inputCase[1], inputCase[2]);
                             break;
 
-                        case "5":
-                            Console.WriteLine("Enter in format,edgeup tailvertex headvertex");
-                            var inputCase5 = Console.ReadLine();
-                            string[] inputFor5 = inputCase5.Split(" ");
-                            operations.EdgeUp(inputFor5[1], inputFor5[2]);
+                        case "edgeup":
+                            operations.EdgeUp(inputCase[1], inputCase[2]);
                             break;
 
-                        case "6":
-                            Console.WriteLine("Enter in format,vertexdown vertex");
-                            var inputCase6 = Console.ReadLine();
-                            string[] inputFor6 = inputCase6.Split(" ");
-                            operations.VertexDown(inputFor6[1]);
+                        case "vertexdown":
+                            operations.VertexDown(inputCase[1]);
                             break;
 
-                        case "7":
-                            Console.WriteLine("Enter in format,vertexup vertex");
-                            var inputCase7 = Console.ReadLine();
-                            string[] inputFor7 = inputCase7.Split(" ");
-                            operations.VertexUp(inputFor7[1]);
+                        case "vertexup":
+                            operations.VertexUp(inputCase[1]);
                             break;
 
-                        case "8":
-                            Console.WriteLine("Enter in format, print, Please note before calling print, you should first build the graph");
-                            var inputCase8 = Console.ReadLine();
-                            if (inputCase8 == "print")
-                            {
-                                operations.Print();
-                            }
-                            else
-                            {
-                                throw new InvalidOperationException("print not entered");
-                            }
+                        case "print":
+                            operations.Print();
                             break;
 
-                        case "100":
-                            Console.WriteLine("Enter quit to exit");
-                            var inputCase100 = Console.ReadLine();
-                            if (inputCase100 == "quit")
-                            {
-                                Console.WriteLine("Thanks for your time, have a good day !!!");
-                                Environment.Exit(0);
-                            }
-
-                            else
-                            {
-                                throw new InvalidOperationException("exit not entered");
-                            }
+                        case "quit":
+                            Environment.Exit(0);
                             break;
                     }
-
-                    option = operations.ChooseOption();
                 }
 
                 catch (Exception ex)
